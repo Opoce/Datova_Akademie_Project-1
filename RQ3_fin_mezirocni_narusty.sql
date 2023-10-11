ec69644c56478ed31238bd6d6a167668e8be78f8
Daniela <137218361+Opoce@users.noreply.github.com>
2023-10-11 11:52:58 +0200

    Add files via upload
    pro zmapování meziročních nárůstů cen v jednotlivých kategoriích produktů, srovnává první a poslední období

---
+++ RQ3_fin_mezirocni_narusty.sql
@@ -0,0 +1,36 @@
+-- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? --
+
+
+-- pro srovnání procentuálních nárůstů ve srovnání průměrné ceny v prvním a posledním roce ve všech kategoriích --
+-- vzestupně --
+
+WITH yearly_data AS (
+    SELECT
+        Food_category_code,
+        Food_category_name,
+        YEAR,
+        AVG(Average_price) AS 'Average_price',
+        ROW_NUMBER() OVER (PARTITION BY Food_category_code 
+        	ORDER BY YEAR) AS row_num_asc,
+        ROW_NUMBER() OVER (PARTITION BY Food_category_code 
+        	ORDER BY YEAR DESC) AS row_num_desc
+    FROM
+    t_daniela_cerna_project_sql_primary_final tdcpspf  
+    GROUP BY
+        Food_category_code, Food_category_name, YEAR
+)
+SELECT
+    Food_category_code,
+    Food_category_name,
+    MIN(year) AS 'First_Year',
+    MAX(year) AS 'Last_Year',
+    AVG(CASE WHEN row_num_asc = 1 THEN Average_price END) AS 'Average_Price_First_Year',
+    AVG(CASE WHEN row_num_desc = 1 THEN Average_price END) AS 'Average_Price_Last_Year',
+    ROUND(((AVG(CASE WHEN row_num_desc = 1 THEN Average_price END) - AVG(CASE WHEN row_num_asc = 1 THEN Average_price END)) 
+    / AVG(CASE WHEN row_num_asc = 1 THEN Average_price END)) * 100, 2) AS 'Percent_Change_First_Last_Year'
+FROM
+    yearly_data
+GROUP BY
+    Food_category_code, Food_category_name
+ORDER BY Percent_Change_First_Last_Year ASC
+;
